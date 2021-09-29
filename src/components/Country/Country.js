410434ec535c89cda93c814d7a668131926c0d55
Akash Islam <akzzislam19194@gmail.com>
2021-09-29 17:14:08 -0500

    second commit

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -5,11 +5,11 @@ const Country = (props) => {
     // console.log(props.Country)
     const { flag, name, capital, population } = props.country
     return (
-        <div className="country">
+        <div className="country bg-dark">
+            <h2>Name : {name}</h2>
             <img src={flag} alt="" />
-            <h2>Name:{name}</h2>
-            <h2>Capita:{capital}</h2>
-            <h2>Population:{population}</h2>
+            <h2>Capital : {capital}</h2>
+            <h2>Population : {population}</h2>
         </div>
     );
 };
